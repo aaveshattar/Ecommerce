0320cf377f1199bd1887ea3a2964c2972071e219
Aavesh Attar <aavesha5@gmail.com>
2025-04-12 22:26:25 +0530

    Initial commit: Project setup

---
+++ src/main/java/com/java/ecommerce/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.java.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.java.ecommerce.response.ApiResponse;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public ApiResponse HomeControllerHandler() {
+		ApiResponse apiResponse=new ApiResponse();
+		apiResponse.setMessage("Welcome to Ecommerce!");
+		return apiResponse;
+	}
+
+}
