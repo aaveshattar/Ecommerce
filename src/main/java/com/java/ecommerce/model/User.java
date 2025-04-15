b2eaf7bcf8eacf4cb19b269128d89a7f81ace259
Aavesh Attar <aavesha5@gmail.com>
2025-04-15 19:34:29 +0530

    entity update

--- src/main/java/com/java/ecommerce/model/User.java
+++ src/main/java/com/java/ecommerce/model/User.java
@@ -1,5 +1,8 @@
 package com.java.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,5 +13,15 @@ public class User {
 	@jakarta.persistence.Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long Id;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String Password;
 
+	private String email;
+	
+	private String fullName;
+	
+	private String mobile;
+	
+	private Enum role;
 }
