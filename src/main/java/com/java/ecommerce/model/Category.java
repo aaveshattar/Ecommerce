a6750fd15459f8f1dbc00fa07f6c9dfa8cba1ec6
Aavesh Attar <aavesha5@gmail.com>
2025-04-16 12:44:34 +0530

    category category

--- src/main/java/com/java/ecommerce/model/Category.java
+++ src/main/java/com/java/ecommerce/model/Category.java
@@ -1,5 +1,33 @@
 package com.java.ecommerce.model;
 
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Category {
 
+	@jakarta.persistence.Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	private String name;
+	
+	@NotNull
+	@Column(unique = true)
+	private String categoryId;
 }
