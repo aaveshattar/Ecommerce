06d7ef77dce080ce86be803b368d0928bc3d40f3
Aavesh Attar <aavesha5@gmail.com>
2025-04-22 19:54:54 +0530

    jwt config

---
+++ src/main/java/com/java/ecommerce/config/JWT_CONSTANT.java
@@ -0,0 +1,8 @@
+package com.java.ecommerce.config;
+
+public class JWT_CONSTANT {
+	
+	public static final String 	SECRET_KEY="sdfghjkjhgfghjkljhgfghjkhgfvebhdugewduewduewdiedweuydvweyfguiwehdhvd";
+	public static final String  JWT_HEADER="Authorization";
+
+}
