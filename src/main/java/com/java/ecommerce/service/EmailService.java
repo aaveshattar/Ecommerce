8bd25a7f8b39717edd3428fee549ceea15a75580
Aavesh Attar <aavesha5@gmail.com>
2025-04-24 22:47:04 +0530

    email config

--- src/main/java/com/java/ecommerce/service/EmailService.java
+++ src/main/java/com/java/ecommerce/service/EmailService.java
@@ -1,8 +1,23 @@
 package com.java.ecommerce.service;
 
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
+	private final JavaMailSender javaMailSender;
+	
+	public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) {
+		
+		try {
+			
+		}catch(MailException e) {
+			
+		}
+	}
 }
