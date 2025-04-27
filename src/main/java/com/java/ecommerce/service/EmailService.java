1dc772e392521d18ebce0f5da0c9dd40983c083a
Aavesh Attar <aavesha5@gmail.com>
2025-04-27 16:01:57 +0530

    email otp

--- src/main/java/com/java/ecommerce/service/EmailService.java
+++ src/main/java/com/java/ecommerce/service/EmailService.java
@@ -3,8 +3,11 @@ package com.java.ecommerce.service;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,10 +16,15 @@ public class EmailService {
 
 	private final JavaMailSender javaMailSender;
 	
-	public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) {
+	public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) throws MessagingException {
 		
 		try {
-			
+			MimeMessage mimeMessage=javaMailSender.createMimeMessage();
+			MimeMessageHelper mimeMessageHelper =new MimeMessageHelper(mimeMessage,"utf-8");
+			mimeMessageHelper.setSubject(subject);
+			mimeMessageHelper.setText(text); 
+			mimeMessageHelper.setTo(userEmail);
+			javaMailSender.send(mimeMessage);
 		}catch(MailException e) {
 			throw new MailSendException("failed to send email");
 		}
